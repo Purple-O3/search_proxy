18c3b5c34edcd55653913fe2944f7308120ca73e
翁 冠 <wengguan333@163.com>
2023-01-12 11:50:35 +0800

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,19 @@
 package main
 
 import (
-	"errors"
-	"path/filepath"
 	"search_proxy/internal/controller"
 	"search_proxy/internal/model/proxy"
 	"search_proxy/internal/objs"
 	"search_proxy/internal/util/log"
-	"strings"
+	"search_proxy/internal/util/viperwrapper"
 
-	"github.com/spf13/viper"
 	_ "go.uber.org/automaxprocs"
 )
 
 func start() {
 	configPath := "../configs/proxy.yaml"
-	fileName := filepath.Base(configPath)
-	fileNames := strings.Split(fileName, ".")
-	if len(fileNames) != 2 {
-		panic(errors.New("fileNames len not equal 2"))
-	}
 	var config objs.Config
-	vp := viper.New()
-	vp.AddConfigPath(filepath.Dir(configPath))
-	vp.SetConfigName(fileNames[0])
-	vp.SetConfigType(fileNames[1])
-	err := vp.ReadInConfig()
-	if err != nil {
-		panic(err)
-	}
-	err = vp.Unmarshal(&config)
+	err := viperwrapper.DecodeConfig(configPath, &config)
 	if err != nil {
 		panic(err)
 	}
